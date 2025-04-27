92dbec2781808fbef01dc341b7d61add2735deaf
Roshni Anand <roshnian@usc.edu>
2025-04-26 19:14:52 -0700

    feat: changed freelist.c

--- freelist.c
+++ freelist.c
@@ -24,6 +24,7 @@
 #define INT_ACCESS_ONCE(var)	((int)(*((volatile int *)&(var))))
 const char *REPLACEMENT_STRAT; //FIFO: This is a variable to store the env variable value.
 int F_HEAD; //FIFO: This variable points to the head/start of the FIFO queue.
+int dummydummy;
 typedef struct
 {
 	/* Spinlock: protects the values below */
